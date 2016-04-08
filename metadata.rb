fa7b530b0aa519193d8191fa9aa7dd13395ed989
Matt Stratton <matt.stratton@gmail.com>
2016-04-08 13:16:55 -0500

    vim and chefdk fix in brew alias

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt.stratton@gmail.com'
 license 'apache2'
 description 'Installs/Configures an OS X workstation for Matt Stratton'
 long_description 'Installs/Configures an OS X workstation for Matt Stratton'
-version '0.3.22'
+version '0.3.23'
 
 depends 'git', '~> 4.2.2'
 depends 'homebrew', '~> 2.1.0'
